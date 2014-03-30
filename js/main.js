65d2d90c7ba2667142b1f2a3d8299d7af96887b2
kunkun <baokundu@gmail.com>
2014-03-30 21:17:37 +0800

    修改RequireJS的timeout加载时间，增加到30秒

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
 
-require.config({
-  baseUrl: '.',
-  paths: {
-    'angular': '//cdn.bootcss.com/angular.js/1.2.0rc3/angular',
-    'angular-route':"//cdn.bootcss.com/angular.js/1.2.0rc3/angular-route",
-    'angular-animate':'//cdn.bootcss.com/angular.js/1.2.0rc3/angular-animate',
-    "ng-sanitize":"//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.0rc3/angular-sanitize.min",
-    'ui-bootstrap':'//cdn.bootcss.com/angular-ui-bootstrap/0.9.0/ui-bootstrap.min'
-  },
-  shim: {
-    'angular' : {'exports' : 'angular'},
-    'angular-route': { deps: ['angular']},
-    'ui-bootstrap': { deps: ['angular']},
-    'angular-animate':{ deps: ['angular']},
-     'ng-sanitize': { deps: ['angular']}
-  }
-});
 
-require(['angular','js/rotuteConfig','js/controller','js/factory','angular-animate',"ui-bootstrap","ng-sanitize"],
+define(['angular','js/rotuteConfig','js/controller','js/factory','angular-animate',"ui-bootstrap","ng-sanitize"],
     function(angular,rotuteConfig,controller,factory){
     var resumeApp = angular.module('resumeApp', ['ngRoute', 'ngAnimate','ui.bootstrap',"ngSanitize"]);
         rotuteConfig.start(resumeApp);
